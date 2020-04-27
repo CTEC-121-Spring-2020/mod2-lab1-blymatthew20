5332756ebe750321516be62f2b5097aeaf7c39b5
Matthew Bly <skyguard14@iCloud.com>
2020-04-27 12:22:40 -0700

    Code for Module 2 lab 1 tested and working

---
+++ Module 2 Lab 1.py
@@ -0,0 +1,57 @@
+"""
+CTEC 121
+Matthew Bly
+Module 2 Lab 1
+Class Demos
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    """
+    A = 2
+    print("\n", A, "\n")
+    a = 22
+    print(a)
+    print()
+
+    AAA = 2
+    print(AAA)
+    
+    spam = 222
+    print()
+    print(spam)
+    print(Spam)
+    print()
+    
+    # expression
+    #print(2)
+    print(3-1)
+    print((3 -1) / 2)
+    print(5/3)
+    print(5//3)
+    print(5%3)
+    print(2 + ((3-1) / 2))
+    
+    print()
+    print("The answer is:", 3 + 4, sep="-", end="")
+    print("end")
+    print()
+
+for i in range(5):
+    print(i, end="\t")
+print()
+"""
+
+# exercise
+classCode = "CTEC 121"
+classCount = 18
+className = "Introduction to Programming and Problem Solving"
+# Welcome to class code>_ The student count is <class count> and the name of the class is <class>
+print("Welcome to ", classCode, ". The student count is ", classCount," and the name of the class is ", className, ".", sep="")
+
+main()    
